58cf4836fe0fc68fb73d07c8523aa6b9ed396d05
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-11-07 20:41:54 +0500

    Task1

---
+++ Tyuiu.SanzyapovOD.Sprint4.Task1.V7.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SanzyapovOD.Sprint4.Task1.V7.Lib
+{
+    public class DataService : ISprint4Task1V7
+    {
+        public int Calculate(int[] array)
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint4.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SanzyapovOD.Sprint4.Task1.V7.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint4.Task1.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 2, 5, 3, 8, 2, 6, 2, 5, 5, 7, 4 };
+            int res = ds.Calculate(numsArray);
+            int wait = 24;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
